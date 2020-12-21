628bc060c109cbe50ac575a29d85de18091a4e0e
Elizabeth Schiffman <68922041+haiwy1218@users.noreply.github.com>
2020-12-21 18:37:06 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,59 +1,148 @@
-// from data.js
-var tableData = data;
+// create the function that gets the data and creates the plots for the id 
+function getPlot(id) {
+    
+    // get the data from the json file
+    d3.json("data/samples.json").then((data)=> {
+        console.log(data)
+        var wfreq = data.metadata.map(d => d.wfreq)
+        console.log(`Washing Freq: ${wfreq}`)
 
-// YOUR CODE HERE!
-var tbody = d3.select("tbody");
-var button = d3.select("#filter-btn");
-var inputField1 = d3.select("#datetime");
-var inputField2 = d3.select("#city");
-var resetbtn = d3.select("#reset-btn");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+        // filter sample values by id 
+        var samples = data.samples.filter(s => s.id.toString() === id)[0];
 
-var populate = (dataInput) => {
+        console.log(samples);
 
-	dataInput.forEach(ufo_sightings => {
-		var row = tbody.append("tr");
-		columns.forEach(column => row.append("td").text(ufo_sightings[column])
-		)
-	});
+        // get only top 10 sample values to plot and reverse for the plotly
+        var sampleValues = samples.sample_values.slice(0, 10).reverse();
+
+        // get only top 10 otu ids for the plot
+        var idValues = (samples.otu_ids.slice(0, 10)).reverse();
+        
+        // get the otu id's to the desired form for the plot
+        var idOtu = idValues.map(d => "OTU " + d)
+
+        console.log(`OTU IDS: ${idOtu}`)
+
+        // get the top 10 labels for the plot
+        var labels = samples.otu_labels.slice(0, 10);
+
+        console.log(`Sample Values: ${sampleValues}`)
+        console.log(`Id Values: ${idValues}`)
+
+        
+        // create trace variable for the plot
+        var trace = {
+            x: sampleValues,
+            y: idOtu,
+            text: labels,
+            type:"bar",
+            orientation: "h",
+        };
+
+        // create data variable
+        var data = [trace];
+
+        // create layout variable to set plots layout
+        var layout = {
+            title: "Top 10 OTU",
+            yaxis:{
+                tickmode:"linear",
+            },
+            margin: {
+                l: 100,
+                r: 100,
+                t: 30,
+                b: 20
+            }
+        };
+
+        // create the bar plot
+        Plotly.newPlot("bar", data, layout);
+
+        //console.log(`ID: ${samples.otu_ids}`)
+        
+        // create the trace for the bubble chart
+        var trace1 = {
+            x: samples.otu_ids,
+            y: samples.sample_values,
+            mode: "markers",
+            marker: {
+                size: samples.sample_values,
+                color: samples.otu_ids
+            },
+            text: samples.otu_labels
+
+        };
+
+        // set the layout for the bubble plot
+        var layout = {
+            xaxis:{title: "OTU ID"},
+            height: 600,
+            width: 1300
+        };
+
+        // create the data variable 
+        var data1 = [trace1];
+
+        // create the bubble plot
+        Plotly.newPlot("bubble", data1, layout); 
+        
+        
+        Plotly.newPlot("gauge", data)
+
+    });    
+}
+    
+// create the function to get the necessary data
+function getInfo(id) {
+    // read the json file to get data
+    d3.json("data/samples.json").then((data)=> {
+        
+        // get the metadata info for the demographic panel
+        var metadata = data.metadata;
+
+        console.log(metadata)
+
+        // filter meta data info by id
+        var result = metadata.filter(meta => meta.id.toString() === id)[0];
+
+        // select demographic panel to put data
+        var demographicInfo = d3.select("#sample-metadata");
+        
+        // empty the demographic info panel each time before getting new id info
+        demographicInfo.html("");
+
+        // grab the necessary demographic data data for the id and append the info to the panel
+        Object.entries(result).forEach((key) => {   
+                demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+        });
+    });
 }
 
-//Populate table
-populate(data);
+// create the function for the change event
+function optionChanged(id) {
+    getPlot(id);
+    getInfo(id);
+}
 
-// Filter by attribute
-button.on("click", () => {
-	d3.event.preventDefault();
-	var inputDate = inputField1.property("value").trim();
-	// Filter by field matching input value
-	var filterDate = data.filter(data => data.datetime === inputDate);
-	console.log(filterDate)
-	//var filterCity = data.filter(data => data.city === inputCity);
-	//console.log(filterCity)
-	//var filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity);
-	//console.log(filterData)
+// create the function for the initial data rendering
+function init() {
+    // select dropdown menu 
+    var dropdown = d3.select("#selDataset");
 
-	// Add filtered sighting to table
-// 	tbody.html("");
+    // read the data 
+    d3.json("data/samples.json").then((data)=> {
+        console.log(data)
 
-// 	let response = {
-// 		filterData, filterCity, filterDate
-// 	}
+        // get the id data to the dropdwown menu
+        data.names.forEach(function(name) {
+            dropdown.append("option").text(name).property("value");
+        });
 
-// 	if (response.filterData.length !== 0) {
-// 		populate(filterData);
-// 	}
-// 		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0))){
-// 			populate(filterCity) || populate(filterDate);
+        // call the functions to display the data and the plots to the page
+        getPlot(data.names[0]);
+        getInfo(data.names[0]);
+    });
+}
 
-// 		}
-// 		else {
-// 			tbody.append("tr").append("td").text("No results found!"); 
-// 		}
-// })
-
-// resetbtn.on("click", () => {
-// 	tbody.html("");
-// 	populate(data)
-// 	console.log("Table reset")
- })
+init();
